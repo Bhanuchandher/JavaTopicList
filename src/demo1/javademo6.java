fa26788ec014325dc000e4aa8656446c3fcbcb34
BhanuB_9936 <bhanu45369@gmail.com>
2022-07-10 23:52:30 +0530

    Changes
    Signed-off-by: BhanuB_9936 <bhanu45369@gmail.com>

--- src/demo1/javademo6.java
+++ src/demo1/javademo6.java
@@ -5,7 +5,7 @@ public class javademo6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hi hello");
+		System.out.println("from source tree");
 
 	}
 
