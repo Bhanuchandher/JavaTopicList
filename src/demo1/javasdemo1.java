97631cd6374d677b75b3fb01bc693e521660b5ca
BhanuB_9936 <bhanu45369@gmail.com>
2022-07-10 20:45:55 +0530

    new code demo1
    Signed-off-by: BhanuB_9936 <bhanu45369@gmail.com>

---
+++ src/demo1/javasdemo1.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class javasdemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Code push from sourcetree");
+
+	}
+
+}
