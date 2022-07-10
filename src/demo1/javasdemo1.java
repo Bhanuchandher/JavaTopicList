8ea89224e416a28c657561da59b35e154200d1d7
BhanuB_9936 <bhanu45369@gmail.com>
2022-07-10 20:48:13 +0530

    one line added
    Signed-off-by: BhanuB_9936 <bhanu45369@gmail.com>

--- src/demo1/javasdemo1.java
+++ src/demo1/javasdemo1.java
@@ -5,6 +5,8 @@ public class javasdemo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Code push from sourcetree");
+		
+		System.out.println("Welcome to source tree");
 
 	}
 
