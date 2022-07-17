56e7081cc794369ae46285632f6ffb8e9b0f55ac
BhanuB_9936 <bhanu45369@gmail.com>
2022-07-17 23:29:43 +0530

    new lines added to javademo7

---
+++ src/demo1/javademo7.java
@@ -0,0 +1,13 @@
+package demo1;
+
+public class javademo7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("add to index");
+		System.out.println("commit changes");
+		System.out.println("push to remote");
+
+	}
+
+}
